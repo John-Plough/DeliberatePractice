e85684173574b60ebe1976ec1e911305c08c3135
John Plough <jplough@gmail.com>
2024-01-17 13:53:16 -0500

    01-vars2 first half

--- dp.js
+++ dp.js
@@ -1,1 +1,21 @@
-// it's real here too
+let first = 'John';
+let last = 'Plough';
+console.log(first + ' ' + last)
+
+let firstName = 'Bruce';
+let lastName = 'Johnson';
+console.log(`${firstName} ${lastName}`);
+
+console.log(`${first} ${last}`)
+
+let word = window.prompt("Enter a word: ")
+if (word == 'marco') {
+    console.log('polo')
+}
+
+let color1 = 'green';
+let color2 = 'blue';
+let color3 = 'purple';
+console.log("My favs are " + color1 + ", " + color2 + ", and " + color3 + ".")
+
+console.log(`I like ${color1}, ${color2}, and ${color3}.`)
--- dp.py
+++ dp.py
@@ -1,1 +1,18 @@
-# it's real
+# first_name = "John"
+# last_name = "Plough"
+# print(first_name + ' ' + last_name)
+
+# f_name = 'john'
+# l_name = 'plough'
+# print(f"{f_name} {l_name}")
+
+# word = input('Please input a word: ')
+# if word == 'marco':
+#     print('polo')
+
+color1 = 'green'
+color2 = 'blue'
+color3 = 'lilac'
+print("My fav colors are " + color1 + ", " + color2 + ", and " + color3 + ".")
+
+print(f"I'll have {color1}, {color2}, and {color3}, please.")
