730e2ccfcef67e90359a2364d4a99ce0a7354099
John Plough <jplough@gmail.com>
2024-01-19 20:11:51 -0500

    # 02-conditionals1 first half

--- dp.py
+++ dp.py
@@ -1,22 +1,58 @@
-# name = input("Please enter a name: ")
-# if name != 'Santa':
-#     print("You're not Santa!")
-# else:
-#     print("Santa!")
+# 02-conditionals1.md
 
-# title = 'The Book'
-# author = 'Drew Tingston'
-# print(title + ' was written by ' + author + '.')
+# Use a variable to store a number, then write a condition that prints 0 if the number is equal to 10, and prints -1 otherwise.
 
-# print(f"It's easier to reference {title} by {author} with string interpolation.")
+num = 8
+if num == 10:
+    print(0)
+else:
+    print(-1)
 
-# pw = input('Please enter a password: ')
-# if pw == 'Joshua':
-#     print('Shall we play a game?')
-# else:
-#     print("Access denied")
+# Use a variable to store a number, then write a condition that prints -1 if the number is less than 10, prints 1 if the number is greater than 10, and prints 0 if the number is equal to 10.
+    
+num = 4
+if num < 10:
+    print(-1)
+elif num > 10:
+    print(1)
+else:
+    print(0)
 
-city1 = 'Austin'
-city2 = 'Boston'
-city3 = 'Calgary'
-print(f"I'd like to visit {city1}, {city2}, and {city3}.")
+# Use variables to store two numbers, then write a condition that prints 1 if the numbers are both less than 10, and prints 0 otherwise.
+
+num1 = 5
+num2 = 8
+if num1 < 10 and num2 < 10:
+    print(1)
+else:
+    print(0)
+
+# Use a variable to store a number, then write a condition that prints 1 if the number is over 9000, and prints -1 otherwise.
+
+num = 100
+if num > 9000:
+    print(1)
+else:
+    print(-1)
+
+# Use a variable to store a number, then write a condition that prints 9 if the number is less than 10, prints 19 if the number is less than 20, prints 29 if the number is less than 30, and prints -1 otherwise (only one print statement should occur).
+
+num = 15
+if num < 10:
+    print(9)
+elif num < 20:
+    print(19)
+elif num < 30:
+    print(29)
+else:
+    print(-1)
+
+# Use variables to store two numbers, then write a condition that prints 100 if either number is greater than 10, and prints -100 otherwise.
+
+# Use a variable to store a number, then write a condition that prints 1776 if the number is less than 0, and prints 1979 otherwise.
+
+# Use a variable to store a number, then write a condition that prints 100 if the number equals 100, prints 99 if the number is equal to 99, and prints 0 otherwise.
+
+# Use variables to store two numbers, then write a condition that prints 1 if the first number is less than zero and the second number is greater than 0, and prints 0 otherwise.
+
+# Use a variable to store a number, then write a condition that prints 5 if the number is greater than 80, prints 4 if the number is greater than 60, prints 3 if the number is greater than 40, prints 2 if the number is greater than 20, and prints 1 otherwise (only one print statement should occur).
