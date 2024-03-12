35e734656ff4d4c9f0948a559c443e781e2e5369
John Plough <jplough@gmail.com>
2024-03-12 01:39:55 -0400

    codewars python

--- dp.py
+++ dp.py
@@ -1,163 +1,182 @@
-# def filter_list(l):
-#     return [el for el in l if isinstance(el, int)]
+# def is_pangram(s):
+#     # loop over string
+#     # if alpha,
+#         # check if in dict --> increment or create
+#     # check length of dict == 26
 
-# def sale_hotdogs(n):
-#     return n * (100 if n < 5 else 95 if n < 10 else 90)
+#     count = {}
+
+#     for char in s:
+#         char = char.lower()
+#         if char.isalpha():
+#             if char in count:
+#                 count[char] += 1
+#             else:
+#                 count[char] = 1
     
-    # if n < 5:
-    #     return 100
-    # elif n < 10:
-    #     return 95
-    # else:
-    #     return 90
+#     print(count)
+#     print(len(count))
 
-# def powers_of_two(n):
-#     return [2**num for num in range(0, n + 1)]
-        
-# def expanded_form(num): # 42
-    #make str --> '42'
-    #loop backwards 10 % int(str[-1])
-    #   100 % int(str[-2])
+#     return len(count) == 26
 
-# def expanded_form(num):
-#     stringed = str(num)
-#     result = ''
+# # is_pangram("The quick brown fox jumps over the lazy dog")
 
-#     for digit in stringed:
-#         result += f"{digit} + "
-#     print(result[:-3])
+# def is_pangram(s):
 
-# expanded_form(42)
+#     counter = {}
 
-# def expanded_form(num):
-#     num = list(str(num))
-#     return ' + '.join(x + '0' * (len(num) - y - 1) for y,x in enumerate(num) if x != '0')
+#     for char in s:
+#         char = char.lower()
+#         if char.isalpha():
+#             counter[char] = counter[char] + 1 if char in counter else 1
 
-# def expanded_form(num):
-#     num = list(str(num)) #num is a list of each digit as a str
-#                          # --> ['7', '9', '2', '6']
-#     return ' + '.join(x + '0' * (len(num) - 1 - y) for y,x in enumerate(num))
+#     return len(counter) == 26
 
 
-# print(isinstance((expanded_form(7926)), str))
+# def is_pangram(s):
 
-# def expanded_form(num):
-#     num = str(num)
-#     return ' + '.join(idx + '0' * (len(num) - digit) for digit,idx in enumerate(num, 1) if idx != '0')
+#     s = s.lower()
 
-# print(expanded_form(5382))
-
-# def expanded_form(num):
-#     return ' + '.join(idx + '0' * (len(str(num)) - digit) for digit,idx in enumerate(str(num), 1) if idx != '0')
-
-# print(expanded_form(98754321))
-
-# def solution(string):
-#     return string[::-1]
-
-# def square(n):
-#     return n**2
-
-# def is_anagram(test, original):
-#     test_dict = {}
-#     orig_dict = {}
-    
-#     for char in test.lower():
-#         if char in test_dict:
-#             test_dict[char] += 1
-#         else:
-#             test_dict[char] = 1
-
-#     for char in original.lower():
-#         if char in orig_dict:
-#             orig_dict[char] += 1
-#         else:
-#             orig_dict[char] = 1
-            
-#     return test_dict == orig_dict
-
-# def is_anagram(test, original):
-#     both = test + original
-#     for char in both:
-#         if test.lower().count(char) == original.lower().count(char):
-#             continue
-#         else:
+#     for char in 'abcdefghijklmnopqrstuvwxyz':
+#         if char not in s:
 #             return False
 #     return True
 
-# def is_anagram(test, original):
-#     print(sorted(test.lower()) == sorted(original.lower()))
+# import string
 
-# is_anagram("foefet", "toffee")
-# is_anagram("Buckethead", "DeathCubeK")
-# is_anagram("beauty", "eauty")
-# is_anagram("eauty", "beauty")
-# is_anagram("Shops", "sHops")
+# def is_pangram(s):
 
-# print(sorted('johnnyplough'))
+#     s = s.lower()
 
-# def fake_bin(x):
-#     result = ''
-#     for digit in x:  
-#         result += '0' if int(digit) < 5 else '1'
-#         print(result)
-#         return result
-
-# def fake_bin(x):
-#     return ''.join('0' if digit < '5' else '1' for digit in x)
-
-# print(fake_bin('3637'))
-
-# li = ['1', '4', '0', '8', '7', '3', '2']
-# print(li)
-# print(sorted(li))
-# print(li)
-# print()
-# print(li)
-# li.sort()
-# print(li)
-
-# def wave(people):
-#     result = []
-#     enum = enumerate([*people]) 
-#         # [(0, 'h'), (1, 'e'), (2, 'l'), (3, 'l'), (4, 'o')]
-#     for idx, letter in enum:
-
-#         result.append(people[:idx] + people[idx].upper() + people[idx+1:])
-#     print(result)
+#     for char in 'abcdefghijklmnopqrstuvwxyz':
+#         if char not in s:
+#             return False
+#     return True
 
 
-# wave('hello')
+# def is_pangram(s):
+#     return set(string.ascii_lowercase).issubset(s.lower())
 
-# change 'hello' to 'heLlo'
-# people[:idx] + people[idx].upper() + people[idx+1:]
+# print(is_pangram("The quick brown fox jumps over the lazy dog"))
 
-def wave(people):
-    # enum = enumerate([people]) 
-        # [(0, 'h'), (1, 'e'), (2, 'l'), (3, 'l'), (4, 'o')]
-    return [people[:idx] + people[idx].upper() + people[idx+1:] for idx, letter in enumerate(people) if letter != ' ']
-    # return [people[:idx] + letter.upper() + people[idx+1:] for idx, letter in enumerate(people) if letter != ' ']
+# def DNA_strand(dna):
+#     comp = ''
+#     for char in dna:
+#         if char == 'A':
+#             comp += 'T'
+#         elif char == 'T':
+#             comp += 'A'
+#         elif char == 'C':
+#             comp += 'G'
+#         elif char == 'G':
+#             comp += 'C'
     
+#     return comp
 
-# print(wave('i do'))
+# def DNA_strand(dna):
 
-# def wave(people):
-#     return [people[:idx] + people[idx].upper() + people[idx+1:] for idx, letter in enumerate(people) if letter != ' ']
+#     table = {
+#         'A': 'T',
+#         'T': 'A',
+#         'C': 'G',
+#         'G': 'C'
+#     }
 
-# def wave(str):
-#     return [str[:i] + str[i:].capitalize() for i in range(len(str)) if str[i] != ' '] 
-#     # return [str[:i] + str[i].upper() + str[i+1:] for i in range(len(str)) if str[i]!=" "]
-# print(wave('i do too'))
+#     print(list(table[key] for key in dna))
+#     return '-'.join(table[key] for key in dna)
 
-# stuff = {
-#     1: 'boat',
-#     'two': 'car',
-#     (3,'four','5'): 'plane'
-# }
+# def DNA_strand(dna):
+#     return dna.translate(str.maketrans("ATCG","TAGC"))
 
-# print(1 in stuff)
-# print('plane' in stuff)
-# print(2 in stuff)
-# print('2' in stuff)
-# print(3 in stuff)
-# print((3, 'four', '5') in stuff)
+# def DNA_strand(dna):
+#     return dna.translate(str.maketrans('ATCG', 'TAGC', 'XYZ'))
+
+# print(DNA_strand('XATAYGTAZ'))
+
+def merge_arrays(arr1, arr2):
+    print(sorted(set(arr1 + arr2)))
+
+merge_arrays([3,4], [1,2,3])
+
+
+
+
+
+
+
+
+isalnum()
+Checks Alphanumeric Character
+isalpha()
+Checks if All Characters are Alphabets
+isdecimal()
+Checks Decimal Characters
+isdigit()
+Checks Digit Characters
+
+islower()
+Checks if all Alphabets in a String are Lowercase
+isnumeric()
+Checks Numeric Characters
+
+
+istitle()
+Checks for Titlecased String
+isupper()
+returns if all characters are uppercase characters
+
+
+
+
+
+maketrans()
+returns a translation table
+
+
+replace()
+Replaces Substring Inside
+
+
+
+
+
+
+split()
+splits string into a list of substrings
+
+join()
+Returns a Concatenated String
+
+
+strip()
+removes both leading and trailing characters
+
+
+
+__
+capitalize()
+Converts first character to Capital Letter
+
+title()
+Returns a Title Cased String
+
+upper()
+returns uppercased string
+
+lower()
+returns lowercased string
+__
+startswith()
+Checks if String Starts with the Specified String
+
+endswith()
+Checks if String Ends with the Specified Suffix
+
+find()
+Returns the index of first occurrence of substring
+
+count()
+returns occurrences of substring in string
+
+index()
+Returns Index of Substring
