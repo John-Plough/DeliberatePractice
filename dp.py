fb7ca3944b98a05de5f0367a9254c159d677e56e
John Plough <jplough@gmail.com>
2024-03-18 04:01:41 -0400

    codewars

--- dp.py
+++ dp.py
@@ -1,271 +1,200 @@
-# def sum_str(a, b):
-#     if a == '':
-#         a = '0'
-#     if b == '':
-#         b = '0'
-#     return str(int(a) + int(b))
+# #func that takes list of ints and a target num
+# #find two different items in list that added together give target num
+# #return indices of those two items in a tuple
+
+# # def two_sum(numbers, target):
+# #     #loop thru list
+# #         #loop thru list again
+# #             #compare el and outer loop el added with target
+# #     pass
+    
+# # def two_sum(numbers, target):
+# #     #loop thru list
+# #         #if target - num in list
+# #             # index of target - num, and num
+# #     pass
+
+# # def two_sum(numbers, target):
+# #     if target % 2 != 0:
+# #         for num in numbers:
+# #             if target - num in numbers:
+# #                 return (numbers.index(target - num), numbers.index(num))
+# #     else:
+# #         if target / 2 in numbers and numbers.lindex(target / 2) != numbers.rindex(target / 2):
+# #             return (numbers.lindex(target / 2), numbers.rindex(target / 2))
+# #         else: 
 
 
-# def sum_str(a, b):
-#     return str(int(a or 0) + int(b or 0))
 
-# def multiple_of_index(arr):
-#     return [num for idx, num in enumerate(arr) if num % idx == 0 and idx > 0]
+# # def two_sum(numbers, target):
+# #     if target / 2 in numbers and numbers.index(target / 2) != numbers[::-1].index(target / 2):
+# #         return (numbers.index(target / 2), numbers[::-1].index(target / 2))
+# #     else:
+# #         for num in numbers:
+# #             if target - num in numbers:
+# #                 return (numbers.index(target - num), numbers.index(num))
+            
 
-# def multiple_of_index(arr):
-#     result = []
 
-#     for idx, num in enumerate(arr):
-#         if idx == 0:
-#             if num == 0:
-#                 result.append(num)
+# # def two_sum(numbers, target):
+# #     half = target / 2
+
+# #     #even target
+# #     if half in numbers and numbers.index(half) != numbers[::-1].index(half):
+# #         return (numbers.index(half), numbers[::-1].index(half))
+# #     else:
+# #         for num in numbers:
+# #             if target - num in numbers:
+# #                 return (numbers.index(target - num), numbers.index(num))
+
+
+# def two_sum(numbers, target):
+    
+#     #odd
+#     if target % 2 != 0:
+#         for num in numbers:
+#             if target - num in numbers:
+#                 return (numbers.index(target - num), numbers.index(num))
+    
+#     #even    
+#     else:
+#         # two occurences of half target (ex. [1,2,2], 4)
+#         half = target / 2
+#         if half in numbers and numbers.count(half) > 1:
+#             first = numbers.index(half)
+#             print(first)
+#             second = numbers[first + 1:].index(half) + 1 + first
+#             print(second)
+#             print(first, second)
+#             return (first, second)
+#         # two different nums
+#         else:
+#             for num in numbers:
+#                 if target - num in numbers:
+#                     return (numbers.index(target - num), numbers.index(num))
+            
+# # print(two_sum([2,2,3], 4))
+
+# #     '1 3 82 44'
+
+# #     [1 ,2, 3]             4          ((0,2), (2,0))
+# #     [1234,5678,9012]      14690      ((1,2), (2,1))
+# #     [2, 2,] 3]             4          ((0,1), (1,0))
+
+
+
+# # [1,1,8], 
+# # [1,1,8], 
+# # [1,2,8]
+
+# # ex = [1,2,4,6,8,9], 8
+# # ex = [1,2,4,4,9], 8
+# # ex = [1,2,3,6,9], 9
+# # ex = [1,2,4,5,6], 9
+# # print(two_sum([1, 2, 3], 4)) # returns (0, 2) or (2, 0)
+# # print(two_sum([3, 2, 4], 6)) # returns (1, 2) or (2, 1))
+                
+
+# def two_sum(numbers, target):
+
+#     for num in numbers:
+#             if target - num in numbers and numbers.index(target - num) != numbers.index(num):
+#                 return (numbers.index(target - num), numbers.index(num))
 #             else:
-#                 continue
-#         elif num % idx == 0:
-#             result.append(num)
+#                 first = numbers.index(target / 2)
+#                 second = numbers[first + 1:].index(half) + first + 1
+#                 return first, second
+    
+#     #even    
+#     else:
+#         # two occurences of half target (ex. [1,2,2], 4)
+#         half = target / 2
+#         if half in numbers and numbers.count(half) > 1:
+#             first = numbers.index(half)
+#             print(first)
+#             second = numbers[first + 1:].index(half) + 1 + first
+#             print(second)
+#             print(first, second)
+#             return (first, second)
+#         # two different nums
+#         else:
+#             for num in numbers:
+#                 if target - num in numbers:
+#                     return (numbers.index(target - num), numbers.index(num))
+            
+# # print(two_sum([2,2,3], 4))
+                
 
-#     return result
+# def two_sum(numbers, target):
+
+#     for num in numbers:
+#         if target - num in numbers and numbers.index(target - num) != numbers.index(num):
+#             return numbers.index(num), numbers.index(target - num)
+
+#     first = numbers.index(target / 2)
+#     second = numbers[first + 1:].index(target / 2) + first + 1
+#     return first, second
+                            
 
 
-# def multiple_of_index(arr):
-#     return [num for idx, num in enumerate(arr) if num == 0 or (idx != 0 and num % idx == 0)]
 
-# # def multiple_of_index(arr):
-# #     return [x for i,x in enumerate(arr) if x==0 or (i!=0 and x%i == 0)]
+# def two_sum(numbers, target):
+#     other = target - num
+#     for num in numbers:
+#         if other in numbers and numbers.index(other) != numbers.index(num):
+#             return numbers.index(num), numbers.index(other)
 
-# print(multiple_of_index([0,0,2]))
+#     first = numbers.index(target / 2)
+#     second = numbers[first + 1:].index(target / 2) + first + 1
+#     return first, second
 
 
-# def is_isogram(string):
-#     my_string = sorted(string.lower())
+# def two_sum(numbers, target):
+    
+#     for num1 in numbers:
+#         num2 = target - num1
+#         if num2 in numbers and num1 != num2:
+#             return numbers.index(num1), numbers.index(num2)
 
-#     i = 0
-#     while i < len(my_string) - 1:
-#         if my_string[i] == my_string[i+1]:
-#             return False
-#         i += 1
-#     return True  
+#     # both addends are same value 
+#     half = target / 2
+#     slice_index = num1 + 1
+#     num1 = numbers.index(half)
+#     num2 = numbers[slice_index:].index(half) + slice_index
+#     return num1, num2
 
-# def is_isogram(string):
-#     lower = string.lower()
-#     for char in lower:
-#         if lower.count(char) != 1:
-#             return False
+# print(two_sum([1234,5678,9012], 14690))
+
+# def two_sum(numbers, target):
+
+#     # addends are different values
+#     for num1 in numbers:
+#         num2 = target - num1
+#         if num2 in numbers and numbers.index(num1) != numbers.index(num2):
+#             return numbers.index(num1), numbers.index(num2)
+
+#     # addends are same value
+#     num1 = numbers.index(target / 2)
+#     num2 = numbers[num1 + 1:].index(target / 2) + num1 + 1
+#     return num1, num2
+
+
+def two_sum(numbers, target):
+    for i, num1 in enumerate(numbers):
+        for j, num2 in enumerate(numbers[i+1:], i+1):
+            if num1 + num2 == target:
+                return i,j
+
+
+def two_sum(numbers, target):
+    other = {}
+
+    for idx, num in enumerate(numbers):
+        if target - num in other:
+            return other[target - num], idx
         
-#     return True
+        other[num] = idx
 
-# def is_isogram(string):
-#     lower = string.lower()
-#     for char in string.lower():
-#         if string.lower().count(char) != 1:
-#             return False
-        
-#     return True
+print(two_sum([1234,5678,9012], 14690))
 
-# is_isogram('bep')
-
-# def is_isogram(string):
-#     return len(string) == len(set(string.lower()))
-
-# def is_isogram(string):
-#     lower = string.lower()
-#     for char in string.lower():
-#         if string.lower().count(char) != 1:
-#             return False
-        
-#     return True
-
-
-# def persistence(n):
-#     if len([*str(n)]) == 1:
-#         return 0
-    
-#     times = 0
-#     while True:
-#         product = 1
-#         for num in [*str(n)]:
-#             product *= int(num)
-
-#         times += 1
-
-#         if product < 10:
-#             break
-
-#         n = product
-#     return times
-
-
-
-# def persistence(n):
-#     n = str(n)
-#     count = 0
-
-#     while len(n) > 1:
-#         product = 1
-#         for digit in n:
-#             product *= int(digit)
-#         n = str(product)
-#         count += 1
-
-#     return count
-
-
-# import operator
-# from functools import reduce
-# def persistence(n):
-#     i = 0
-#     while n>=10:
-#         n=reduce(operator.mul,[int(x) for x in str(n)],1)
-#         i+=1
-#     return i
-
-
-# import operator
-# from functools import reduce
-# def persistence(num):
-#     count = 0
-#     while num > 9:
-#         num = reduce(operator.mul, [int(digit) for digit in str(num)], 1)
-#         count += 1
-#     return count
-
-
-# import operator
-# from functools import reduce
-# def persistence(num):
-#     count = 0
-
-#     while num > 9:
-#         num = reduce(operator.mul, [int(digit) for digit in str(num)], 1)
-#         count += 1
-
-#     return count
-
-# print(persistence(999))
-
-# def tower_builder(n):
-#     tower = []
-#     i = 1
-#     stars = 1
-
-#     for row in range(1, n+1):
-#         spaces = n - i
-
-#         tower.append((spaces * ' ') + (stars * '*') + (spaces * ' '))
-#         i += 1
-#         stars += 2
-
-#     return tower
-
-# def tower_builder(n):
-#     result = []
-    
-#     i = 2 * n - 1 
-#     j = 1
-#     spaces = i * ' ' 
-#     stars = j * '*'
-
-#     for row in range(1, n+1):
-#         result.append((spaces * ' ') + (stars * '*') + (spaces * ' '))
-#         spaces -= 2
-#         stars += 2
-
-#     print(result)
-#     return result
-
-# tower_builder(6)
-
-
-# def tower_builder(n):
-#     tower = []
-#     stars = 1
-#     spaces = n - 1
-
-#     for _ in range(0, n):
-#         tower.append((spaces * ' ') + (stars * '*') + (spaces * ' '))
-#         stars += 2
-#         spaces -= 1
-
-#     return tower
-
-
-
-# def tower_builder(floors):
-#     result = []
-
-#     for floor in range(1, floors+1):
-#         stars = '*' * (2 * floor - 1)
-#         space = ' ' * (floors - floor)
-#         result.append(space + stars + space)
-    
-#     return result
-
-
-
-# def tower_builder(floors):
-#     return [('*' * (2 * floor - 1)).center(floors * 2 - 1) for floor in range(1, floors + 1)]
-
-# # def tower_builder(floors):
-# #     result = []
-
-# #     for floor in range(1, floors+1):
-# #         stars = '*' * (2 * floor - 1)
-# #         space = ' ' * (floors - floor)
-# #         result.append(space + stars + space)
-    
-# #     return result
-
-# print(tower_builder(4))
-
-
-# def multi_table(number):
-#     result = ''
-    
-#     for row in range(1, 11):
-#         result += f"{row} * {number} = {row * number}\n"
-
-#     return result.rstrip()
-
-
-# def multi_table(number):
-#     return '\n'.join(f"{row} * {number} = {row * number}" for row in range(1, 11))
-
-# print(multi_table(0))
-
-
-# def human_years_cat_years_dog_years(human_years):
-#     cat = 0
-#     dog = 0
-    
-#     if human_years > 0:
-#         cat += 15
-#         dog += 15
-    
-#     if human_years > 1:
-#         cat += 9
-#         dog += 9
-
-#     if human_years > 2:
-#         cat += (human_years - 2) * 4
-#         dog += (human_years - 2) * 5
-
-#     return[human_years, cat, dog]
-
-# def human_years_cat_years_dog_years(human):
-#     cat = 15 * (human > 0) + 9 * (human > 1) + 4 * (human - 2) * (human > 2)
-#     dog = 15 * (human > 0) + 9 * (human > 1) + 5 * (human - 2) * (human > 2)
-#     return[human, cat, dog]
-
-
-
-
-def count_sheep(n):
-    return ''.join(f"{sheep_number} sheep..." for sheep_number in range(1, n+1))
-
-
-def count_sheep(n):
-    murmur = ''
-    for sheep in range(1, n + 1):
-        murmur += f"{sheep} sheep..."
-
-    return murmur
+arr = [1,3,6,15]
